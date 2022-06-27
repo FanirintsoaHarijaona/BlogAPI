eb956bd5db57ee698849d685c4a224881a6af398
Faniry Aomin <fanirintsoaharijaona@gmail.com>
2022-06-27 09:20:48 +0300

    Using viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from rest_framework import viewsets
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer,UserSerializer
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
 # Create your views here.
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-   permission_classes   =   (IsAuthorOrReadOnly,)
-   queryset             =   Post.objects.all()
-   serializer_class     =   PostSerializer
+class PostViewSet(viewsets.ModelViewSet):
+   permission_class           =  (IsAuthorOrReadOnly,)
+   queryset                   =  Post.objects.all()
+   serializer_class           =  PostSerializer
 
-class PostList(generics.ListCreateAPIView):
-   queryset            =   Post.objects.all() 
-   serializer_class    =   PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+   queryset                   =  get_user_model().objects.all()
+   serializer_class           =  UserSerializer
